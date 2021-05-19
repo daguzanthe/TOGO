b61ede3772cd9cf18edab0d3da1941636d3c66e5
Mathilde Hamon <mathilde.hamon@eisti.fr>
2021-05-19 14:35:01 +0200

    footer

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
 </head>
 <body>
     <?php include('./php/general/header.php') ?>
-    <h1>test</h1>
+    <div class="container">
+    <div class="row" >
+        <div class="col-sm-5">
+            <h3>Qui sommes nous ?</h3>
+            <hr>
+            <p>Notre ambition est d’apporter à vous, opérateurs économiques, organisations réligieuses / ONG, ménages togolais des solutions adaptées à vos besoins dans nos domaines de compétence afin de vous rendre plus performants.</p>
+        </div>
+        <div class="col-sm-5">
+            <h3>Ou sommes nous ?</h3>
+            <hr>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.289438430695!2d1.1919776147691656!3d6.225516295493468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x102159315a13e671%3A0x3854e6ed167f2f03!2sTogo%20Beta!5e0!3m2!1sfr!2stg!4v1621424961045!5m2!1sfr!2stg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+    </div>
+    <?php include('./php/general/footer.php') ?>
 </body>
 </html>
---
+++ php/general/footer.php
@@ -0,0 +1,27 @@
+<footer class="text-center text-white" style="background-color: #f1f1f1;">
+  <!-- Grid container -->
+  <div class="container pt-4">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a href="https://www.facebook.com/togobeta"><img src="../../images/fac.png" alt="" width=4%></a>
+
+      <!-- Youtube -->
+      <a href="https://www.youtube.com/channel/UCWpZDDCOXdkm42P6NCfZr2Q"><img src="../../images/yout.png" alt="" width=4%></a>
+     
+
+     <!-- Whatsapp -->
+     <img src="../../images/what.png" alt="" width=4%>
+
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2021 Copyright:
+    <a class="text-dark" href="https://togobeta.com/">TogoBeta.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
