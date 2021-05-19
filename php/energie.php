ab49f53ba5d5a620a3c0173b8445412930cd64dc
penarandaa <penarandaa@cy-tech.fr>
2021-05-19 11:57:26 +0000

    header1

---
+++ php/energie.php
@@ -0,0 +1,16 @@
+<html lang="fr">
+<head>
+  <link rel="icon" href= "img/enedisLogo.png"  media="screen">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <title>Page d'accueil</title>
+</head>
+
+<body>
+ <?php /*include 'php/header.php'; */?>
+  <div class="nomPage">
+  </div>
+
+  <?php /*include 'php/footer.php'; */?>
+</body>
+
+</html>
---
+++ php/general/header.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3 w-100">
+    
+    <div class="collapse navbar-collapse d-flex" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item ">
+                <a class="nav-link" href="../index.php">Accueil</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="../index.php">Energie</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="../index.php">Telecom</a>
+            </li>
+        </ul>
+</nav>
