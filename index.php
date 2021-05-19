df59d0a4f58288c0bfa882a3133e88f862d30f96
penarandaa <penarandaa@cy-tech.fr>
2021-05-19 15:39:26 +0000

    Où

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 <p>Notre ambition est d’apporter à vous, opérateurs économiques, organisations réligieuses / ONG, ménages togolais des solutions adaptées à vos besoins dans nos domaines de compétence afin de vous rendre plus performants.</p>
             </div>
             <div class="col-sm-5">
-                <h3>Ou sommes nous ?</h3>
+                <h3>Où sommes nous ?</h3>
                 <hr>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.289438430695!2d1.1919776147691656!3d6.225516295493468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x102159315a13e671%3A0x3854e6ed167f2f03!2sTogo%20Beta!5e0!3m2!1sfr!2stg!4v1621424961045!5m2!1sfr!2stg"  class="map" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
